21c7e5c35882d7ababc5dfa33ec4606dd0c49619
dudtjq <fdf98952@gmail.com>
2024-06-10 18:18:14 +0900

    entity 테이블 작성 완료

--- src/main/java/com/ict_final/issuetrend/entity/User.java
+++ src/main/java/com/ict_final/issuetrend/entity/User.java
@@ -1,4 +1,60 @@
 package com.ict_final.issuetrend.entity;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "tbl_user")
 public class User {
+    //회원번호
+    @Id
+    @Column(name = "user_no")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String userNo;
+
+    //프로필 사진
+    @Column(name = "profile_image", length = 1000)
+    private String profileImage;
+
+    //아이디(이메일)
+    @Size(max = 32)
+    @Email
+    @Column(nullable = false, unique = true,  length = 100)
+    private String email;
+
+    //비밀번호
+    @Size(max = 20)
+    @Column(nullable = false, length = 50)
+    private String password;
+
+    //핸드폰 번호
+    @Size(max = 11)
+    @Column(name = "phone_num",length = 20, nullable = false)
+    private String phoneNum;
+
+    //지역명
+    @Column(name = "region_name", length = 20,nullable = false)
+    private String regionName;
+
+    //관심키워드
+    @Column(name = "favorite_keyword", length = 20)
+    private String favoriteKeyword;
+
+    //로그인경로
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    @Column(name = "login_path", length = 20)
+    private LoginPath loginPath = LoginPath.COMMON;
+
+
 }
