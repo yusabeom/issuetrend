cf6bcdd7fa21e7b4c43ed9309688afb202f5cb64
sabeom <tkqja717@naver.com>
2024-06-17 14:49:18 +0900

    ArticleDetailResponseDTO 필드 TruncatedText 추가

--- src/main/java/com/ict_final/issuetrend/dto/response/ArticleDetailResponseDTO.java
+++ src/main/java/com/ict_final/issuetrend/dto/response/ArticleDetailResponseDTO.java
@@ -3,7 +3,9 @@ package com.ict_final.issuetrend.dto.response;
 import com.ict_final.issuetrend.entity.Article;
 import lombok.*;
 
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @Getter
 @ToString
@@ -21,6 +23,7 @@ public class ArticleDetailResponseDTO {
     private String writer;
     private String img;
     private String articleLink;
+    private String truncatedText; // 본문 앞부분 자른 내용
 
     public ArticleDetailResponseDTO(Article article) {
         this.articleCode = article.getArticleCode();
@@ -31,5 +34,28 @@ public class ArticleDetailResponseDTO {
         this.writer = article.getWriter();
         this.img = article.getImg();
         this.articleLink = article.getArticleLink();
+        this.truncatedText = truncateText(article.getText(), 100);
     }
+
+    // 한글 바이트 깨짐 방지 100바이트로 자르는 메서드
+    private String truncateText(String text, int maxLength) {
+        if (text != null && text.length() > maxLength) {
+            // maxLength 길이를 초과할 때만 자르기
+            StringBuilder sb = new StringBuilder();
+            int currentLength = 0;
+            for (char c : text.toCharArray()) {
+                // 각 문자의 바이트 길이 확인
+                byte[] bytes = String.valueOf(c).getBytes(StandardCharsets.UTF_8);
+                if (currentLength + bytes.length > maxLength) {
+                    break;
+                }
+                sb.append(c);
+                currentLength += bytes.length;
+            }
+            return sb.toString() + (currentLength < text.length() ? "..." : "");
+        }
+        // 본문이 100바이트를 넘지 않는다면 기존 text를 리턴
+        return text;
+    }
+
 }
