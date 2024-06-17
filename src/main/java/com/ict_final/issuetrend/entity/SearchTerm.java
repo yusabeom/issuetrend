ec46af868fb785888cbc8b7633aaf6cfa09a96e9
yb <yb725v@naver.com>
2024-06-17 15:50:38 +0900

    DB 수정

---
+++ src/main/java/com/ict_final/issuetrend/entity/SearchTerm.java
@@ -0,0 +1,27 @@
+package com.ict_final.issuetrend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "tbl_search_term")
+public class SearchTerm {
+
+    //검색어 번호
+    @Id
+    @Column(name = "search_no")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long searchNo;
+
+    //검색어
+    @Column(name = "search_term", length = 50)
+    private String searchTerm;
+
+}
