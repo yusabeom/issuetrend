93cc1c2833b86286d75319e622841c46fde62d79
yb <yb725v@naver.com>
2024-06-20 14:40:48 +0900

    닉네임, 스크랩 DB 추가

---
+++ src/main/java/com/ict_final/issuetrend/entity/Scrap.java
@@ -0,0 +1,33 @@
+package com.ict_final.issuetrend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "tbl_scrap")
+public class Scrap {
+
+    //스크랩 번호
+    @Id
+    @Column(name = "scrap_no")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long scrapNo;
+
+    //회원 번호
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_no")
+    private User user;
+
+    //게시글 번호
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_no")
+    private BoardPost boardPost;
+
+}
