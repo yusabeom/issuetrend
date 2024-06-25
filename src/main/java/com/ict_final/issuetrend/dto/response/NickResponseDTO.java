d5f8d5b2ad0c4aaea9413601f75455d1fc16461f
sabeom <tkqja717@naver.com>
2024-06-25 11:46:31 +0900

    nickDTO profileImage 필드 추가

--- src/main/java/com/ict_final/issuetrend/dto/response/NickResponseDTO.java
+++ src/main/java/com/ict_final/issuetrend/dto/response/NickResponseDTO.java
@@ -14,11 +14,13 @@ public class NickResponseDTO {
     private String nickname;
     private String email;
     private String regionName;
+    private String profileImage;
 
     public NickResponseDTO(User user) {
         this.userNo = user.getUserNo();
         this.nickname = user.getNickname();
         this.email = user.getEmail();
         this.regionName = user.getRegionName();
+        this.profileImage = user.getProfileImage();
     }
 }
